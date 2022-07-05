7590b9f0e1eee54cfed3f46b10ffecde3d155847
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-05 17:23:02 +0000

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Fredrick Rana';
+var isCool = true;
+var totalPets = 4;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof null);
+
+var never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
