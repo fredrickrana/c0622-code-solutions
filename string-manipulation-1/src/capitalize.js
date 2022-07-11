4cc907c19c30b92832237e2279590cb7d49ed171
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-11 20:28:18 +0000

    simplified capitalize function solution. changed pseudo code.

--- string-manipulation-1/src/capitalize.js
+++ string-manipulation-1/src/capitalize.js
@@ -2,7 +2,7 @@
 
 // use the touppercase method to capitalize the first letter of the word
 // if the word has a length greater than one, use a loop to check each individual character
-// take the value of each character an use the tolowercase method to lower case each character
+// take the value of each character and use the tolowercase method to lower case each character
 // add each character to the first uppercased character
 // return the word from the function
 
