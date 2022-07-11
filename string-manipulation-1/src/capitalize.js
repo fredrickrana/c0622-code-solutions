55512d2a2616da40f71f17c58aca821a4b7cb856
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-11 19:34:54 +0000

    in progress

--- string-manipulation-1/src/capitalize.js
+++ string-manipulation-1/src/capitalize.js
@@ -1,39 +1,17 @@
 /* exported capitalize */
 
-// assign an empty string to store data and use as an output
-// check each character in the string
-// the value of a specific character in the string is determined through applying indexes
-// the index [0] represents the first character of a string, therefore, if the character that is being checked is the first character, use the toUpperCase method to capitalize that character
-// concatenate the value with the empty string
-// if it is any other value aside from index[0] (the first character of the string), then use the toLowerCase method to lowercase all letters
-// concatenate all the following values onto the string
-// return the string from the function
+// use the touppercase method to capitalize the first letter of the word
+// if the word has a length greater than one, use a loop to check each individual character
+// take the value of each character an use the tolowercase method to lower case each character
+// add each character to the first uppercased character
+// return the word from the function
 
 function capitalize(word) {
-  var alphabet = '';
-  for (var i = 0; i < word.length; i++) {
-    if (i === 0) {
-      var firstChar = word[i].toUpperCase();
-      alphabet += firstChar;
-    } else if (i > 0) {
-      var allChar = word[i].toLowerCase();
-      alphabet += allChar;
+  var alphabet = word[0].toUpperCase();
+  if (word.length > 1) {
+    for (var i = 1; i < word.length; i++) {
+      alphabet += word[i].toLowerCase();
     }
   }
   return alphabet;
 }
-
-// alternate solution, attempted to make function more efficient
-
-// function capitalize(word) {
-//   if (word.length === 1) {
-//     return word.toUpperCase();
-//   } else {
-//     var lower = word.toLowerCase();
-//     var upper = lower[0].toUpperCase();
-//     for (var i = 1; i < lower.length; i++) {
-//       var fullWord = upper += lower[i];
-//     }
-//     return fullWord;
-//   }
-// }
