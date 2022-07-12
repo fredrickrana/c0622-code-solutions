48e3a24576635ab04e278750d737ed6cdada7613
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-12 00:47:00 +0000

    in progress

---
+++ string-manipulation-2/src/capitalize-words.js
@@ -0,0 +1,9 @@
+/* exported capitalizeWords */
+
+// create an empty string for data storage and output
+// use the split method to separate the string by space
+// go through each element in the
+
+// use split method with string space
+// use for loop to capitalize the first character of each element
+// add each element to empty string
