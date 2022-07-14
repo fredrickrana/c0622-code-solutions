ab6c5dead22cf04ce76ec9e6cd2f56d340b061a6
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-14 22:08:52 +0000

    in progress

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,19 @@
+/* exported pick */
+
+// create an empty object for data storage and input
+// loop through the array elements in keys
+// loop through the properties in the source object
+// if the element in the array matches the property in the object,  add the property and its value onto the empty object
+// return the object from the function
+
+// function pick(source, keys) {
+//   var object = {};
+//   for (var element in keys) {
+//     for (var property in source) {
+//       if (keys[element] === property) {
+//         object[property] = source[keys[element]];
+//       }
+//     }
+//   }
+//   return object;
+// }
