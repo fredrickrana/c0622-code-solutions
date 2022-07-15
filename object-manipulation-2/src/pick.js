86b6bded9cc5a8b154449e29959d0c16feca9032
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-15 17:03:15 +0000

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -15,5 +15,16 @@
 //       }
 //     }
 //   }
+//   console.log(object);
+//   return object;
+// }
+
+// function pick(source, keys) {
+//   var object = {};
+//   for (var i = 0; i < keys.length; i++) {
+//     if (source[keys[i]] !== undefined) {
+//       object[keys[i]] = source[keys[i]];
+//     }
+//   }
 //   return object;
 // }
