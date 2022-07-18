87b0172af5f81f200e0fc3e366df7b3707a7435d
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-18 08:50:19 +0000

    completed object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,7 +1,15 @@
 /* exported defaults */
 
-//
+// the function would modify the target object
+// go through each of the properties in the source object
+// if the property in the source object is not equal to the property in the target object and if the property in the target object is undefined, add the property from the source object and its value to the target object
+// return the target object from the function
 
 function defaults(target, source) {
-
+  for (var property in source) {
+    if (property !== target[property] && target[property] === undefined) {
+      target[property] = source[property];
+    }
+  }
+  return target;
 }
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -2,9 +2,21 @@
 
 // create an empty object for data storage and input
 // loop through the array elements in keys
+// if the element within the keys array matches with a property in the source object (if it is not undefined), add its value into the empty object
 // loop through the properties in the source object
-// if the element in the array matches the property in the object,  add the property and its value onto the empty object
 // return the object from the function
+
+function pick(source, keys) {
+  var object = {};
+  for (var i = 0; i < keys.length; i++) {
+    if (source[keys[i]] !== undefined) {
+      object[keys[i]] = source[keys[i]];
+    }
+  }
+  return object;
+}
+
+// ALTERNATE SOLUTION. Debugger and console.log show correct output for all tests.
 
 // function pick(source, keys) {
 //   var object = {};
@@ -16,15 +28,5 @@
 //     }
 //   }
 //   console.log(object);
-//   return object;
-// }
-
-// function pick(source, keys) {
-//   var object = {};
-//   for (var i = 0; i < keys.length; i++) {
-//     if (source[keys[i]] !== undefined) {
-//       object[keys[i]] = source[keys[i]];
-//     }
-//   }
 //   return object;
 // }
