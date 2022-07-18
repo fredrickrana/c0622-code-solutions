38e69ad70353bdab2dc9e6a886762ebaf518f18c
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-18 16:00:54 +0000

    completed string-manipulation-2

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,11 @@
 /* exported lastChars */
+
+// in order to eliminate characters in a string, use the slice method
+// the slice method of the string object would be called with two arguments
+// the first argument (which is the start of the first letter to include)
+// since we are starting from right to left (the last character) we would use negative length as the argument in order to count from the end
+// return the method call from the function
+
+function lastChars(length, string) {
+  return string.slice(-length);
+}
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,18 @@
 /* exported swapChars */
+
+// swap the characters on the string based on the indexes that are given
+// store in a variable the characters of the indexes that need to be switched
+// use the split method to turn the string into an array
+// once an array, use the splice method with three arguments (start, deleteCount, item1)
+// this will replace the characters with each index
+// use the join method to join the array together to recreate the string
+// return the string from the function
+
+function swapChars(firstIndex, secondIndex, string) {
+  var one = string[firstIndex];
+  var two = string[secondIndex];
+  var array = string.split('');
+  array.splice(firstIndex, 1, two);
+  array.splice(secondIndex, 1, one);
+  return array.join('');
+}
