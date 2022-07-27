ebe8f384b63836b1bccb796f99fc319eb3e12d17
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-27 22:38:07 +0000

    completed javascript-carousel

---
+++ javascript-carousel/main.js
@@ -0,0 +1,63 @@
+var $leftArrow = document.querySelector('.fa-angle-left');
+var $rightArrow = document.querySelector('.fa-angle-right');
+var $pokemonImage = document.querySelector('.pokemon-image');
+var $pokemonArray = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
+var $circlesRow = document.querySelector('.all-circles');
+var $allCircles = document.querySelectorAll('.circle');
+var $carousel = setInterval(moveRight, 3000);
+
+function moveLeft(event) {
+  for (var i = $pokemonArray.length - 1; i >= 0; i--) {
+    if ($pokemonImage.getAttribute('src') === 'images/001.png') {
+      $pokemonImage.setAttribute('src', $pokemonArray[i]);
+      $allCircles[i].className = 'fas fa-circle circle';
+      $allCircles[0].className = 'far fa-circle circle';
+      resetInterval();
+      return;
+    } else if ($pokemonImage.getAttribute('src') === $pokemonArray[i]) {
+      $pokemonImage.setAttribute('src', $pokemonArray[i - 1]);
+      $allCircles[i - 1].className = 'fas fa-circle circle';
+      $allCircles[i].className = 'far fa-circle circle';
+      resetInterval();
+      return;
+    }
+  }
+}
+$leftArrow.addEventListener('click', moveLeft);
+
+function moveRight(event) {
+  for (var i = 0; i < $pokemonArray.length; i++) {
+    if ($pokemonImage.getAttribute('src') === 'images/039.png') {
+      $pokemonImage.setAttribute('src', $pokemonArray[i]);
+      $allCircles[i].className = 'fas fa-circle circle';
+      $allCircles[4].className = 'far fa-circle circle';
+      resetInterval();
+      return;
+    } else if ($pokemonImage.getAttribute('src') === $pokemonArray[i]) {
+      $pokemonImage.setAttribute('src', $pokemonArray[i + 1]);
+      $allCircles[i + 1].className = 'fas fa-circle circle';
+      $allCircles[i].className = 'far fa-circle circle';
+      resetInterval();
+      return;
+    }
+  }
+}
+$rightArrow.addEventListener('click', moveRight);
+
+function switchCircles(event) {
+  for (var i = 0; i < $allCircles.length; i++) {
+    if (event.target === $allCircles[i]) {
+      $allCircles[i].className = 'fas fa-circle circle';
+      $pokemonImage.setAttribute('src', $pokemonArray[i]);
+    } else {
+      $allCircles[i].className = 'far fa-circle circle';
+    }
+  }
+  resetInterval();
+}
+$circlesRow.addEventListener('click', switchCircles);
+
+function resetInterval() {
+  clearInterval($carousel);
+  $carousel = setInterval(moveRight, 3000);
+}
