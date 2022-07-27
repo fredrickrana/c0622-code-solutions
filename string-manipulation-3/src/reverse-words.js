006faa8b36b44676830249bd7f1508419440d33d
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-27 18:27:24 +0000

    in progress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,5 +1,20 @@
 /* exported reverseWords */
 
 // function reverseWords(string) {
-
+//   var backwards = '';
+//   // for (var i = string.length - 1; i >= 0; i--) {
+//   //   backwards += string[i];
+//   // }
+//   // return backwards;
+//   for (var i = 0; string[i] !== ' '; i++) {
+//     backwards += string[i];
+//     i++;
+//     for (var x = backwards.length; x >= 0; x--) {
+//       backwards += string[i];
+//     }
+//   }
+//   return backwards;
 // }
+
+// take a word and put in reverse
+//
