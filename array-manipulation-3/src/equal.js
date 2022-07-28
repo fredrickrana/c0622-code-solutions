3651313155a586994aea14a013c5f4f4e3c360a9
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-28 17:03:50 +0000

    in progress

---
+++ array-manipulation-3/src/equal.js
@@ -0,0 +1,25 @@
+/* exported equal */
+
+// function equal(first, second) {
+//   if (first === second) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function equal(first, second) {
+//   var firstArray = [];
+//   var secondArray = [];
+//   for (var i = 0; i < first.length; i++) {
+//     firstArray.push(first[i]);
+//   }
+//   for (var x = 0; x < second.length; x++) {
+//     secondArray.push(second[x]);
+//   }
+//   if (firstArray !== secondArray) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
---
+++ array-manipulation-3/src/flatten.js
@@ -0,0 +1,6 @@
+/* exported flatten */
+
+// function flatten(array) {
+//   var unitedArray = [];
+//   for (var i = 0; )
+// }
