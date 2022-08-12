e4f8ca554b3321d8aeda89e03b47111bda40628b
Fredrick Rana <fredrickrana01@gmail.com>
2022-08-12 20:01:43 +0000

    completed array-manipulation-3

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,19 @@
 /* exported intersection */
+
+// create an empty array for output and storing elements
+// go through each element within the first array
+// for each element in the first array, compare the value to all the elements in the second array
+// if a value matches the element in the first array with an element in the second array, push the element into the intial empty array
+// return the initial empty array from the function
+
+function intersection(first, second) {
+  var match = [];
+  for (var i = 0; i < first.length; i++) {
+    for (var x = 0; x < second.length; x++) {
+      if (first[i] === second[x]) {
+        match.push(first[i]);
+      }
+    }
+  }
+  return match;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,38 @@
 /* exported zip */
+
+// make sure that the length of both arrays are equal to each other
+// if both arrays are not equal to each other, delete the last element of the array with greater length until both lengths are equal
+// if first array is larger, delete until it matches length of second array
+// if second array is larger, delete until it matches length of the first array
+// once arrays have equal length, create three empty arrays
+// the first array is going to store the values found within the first array
+// the second array is going to store the values found within the second array
+// the third array will be storing all values and will be returned from the function
+// check each element within the first array, and check the value of the second array that has the same index
+// add each value to one array (first and second)
+// combine the elements within each array and store into a seperate array (third)
+// delete the elements of the previous arrays (first and second) so that it returns back to an empty array
+// continue to add all elements to the third array
+// return the third array from the function
+
+function zip(first, second) {
+  if (first.length !== second.length) {
+    if (first.length > second.length) {
+      first.splice(first.length - 1);
+    } else if (second.length > first.length) {
+      second.splice(second.length - 1);
+    }
+  }
+  var combine = [];
+  var combineTwo = [];
+  var combineThree = [];
+  for (var i = 0; i < first.length; i++) {
+    combine.push(first[i]);
+    combineTwo.push(second[i]);
+    var arrayThree = combine.concat(combineTwo);
+    combineThree.push(arrayThree);
+    combine.splice(0);
+    combineTwo.splice(0);
+  }
+  return combineThree;
+}
