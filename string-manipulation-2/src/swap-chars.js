20f65562c15c61119e2ba1b26d3da68425f16eed
Fredrick Rana <fredrickrana01@gmail.com>
2022-08-12 23:40:30 +0000

    modified swapChars function to solve algorithm without altering the string into an array

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,18 +1,23 @@
 /* exported swapChars */
 
-// swap the characters on the string based on the indexes that are given
-// store in a variable the characters of the indexes that need to be switched
-// use the split method to turn the string into an array
-// once an array, use the splice method with three arguments (start, deleteCount, item1)
-// this will replace the characters with each index
-// use the join method to join the array together to recreate the string
-// return the string from the function
+// create an empty string for data storage and output
+// go through each character in the string
+// concatenate each character to the initial empty string
+// once you reach the character within the firstIndex, concatenate the value of the secondIndex within the string
+// once you reach the character within the secondIndex, concatenate the value of the firstIndex within the string
+// continue until the end of the string length
+// return the initial empty string from the function
 
 function swapChars(firstIndex, secondIndex, string) {
-  var one = string[firstIndex];
-  var two = string[secondIndex];
-  var array = string.split('');
-  array.splice(firstIndex, 1, two);
-  array.splice(secondIndex, 1, one);
-  return array.join('');
+  var newString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (firstIndex === i) {
+      newString += string[secondIndex];
+    } else if (secondIndex === i) {
+      newString += string[firstIndex];
+    } else {
+      newString += string[i];
+    }
+  }
+  return newString;
 }
