44599f1893c17bfc71385e9c28d5a594ed29d28b
Fredrick Rana <fredrickrana01@gmail.com>
2022-08-18 00:02:49 +0000

    commented out cat.js attempt

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-for (var i = 2; i < process.argv.length; i++) {
-  const file = process.argv[i];
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-}
+// for (var i = 2; i < process.argv.length; i++) {
+//   const file = process.argv[i];
+//   fs.readFile(file, 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   });
+// }
