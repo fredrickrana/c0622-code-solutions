bf757bfd3159a35380666de63d7ae1373059f6a0
Fredrick Rana <fredrickrana01@gmail.com>
2022-08-18 19:50:23 +0000

    completed node-notes-cli

---
+++ node-notes-cli/app.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const dataJSON = require('./data.json');
+const action = process.argv[2];
+
+// node app.js read
+if (action === 'read') {
+  for (const id in dataJSON.notes) {
+    console.log(`${id}: ${dataJSON.notes[id]}`);
+  }
+}
+
+// node app.js create
+if (action === 'create') {
+  const notes = dataJSON.notes;
+  notes[dataJSON.nextId] = process.argv[3];
+  fs.writeFile('./data.json', JSON.stringify(dataJSON, null, 2), err => {
+    if (err) throw err;
+  });
+}
+
+// node app.js update
+if (action === 'update') {
+  for (var id in dataJSON.notes) {
+    if (process.argv[3] === id) {
+      const notes = dataJSON.notes;
+      notes[id] = process.argv[4];
+      fs.writeFile('./data.json', JSON.stringify(dataJSON, null, 2), err => {
+        if (err) throw err;
+      });
+    }
+  }
+}
+
+// node app.js delete
+if (action === 'delete') {
+  delete dataJSON.notes[process.argv[3]];
+  fs.writeFile('./data.json', JSON.stringify(dataJSON, null, 2), err => {
+    if (err) throw err;
+  });
+}
