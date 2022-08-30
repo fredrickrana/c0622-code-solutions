62a74c096c5d7526c68bdb00bc32a62e9fcf4f61
Fredrick Rana <fredrickrana01@gmail.com>
2022-08-30 23:37:03 +0000

    completed react-class-components

--- react-class-components/src/index.jsx
+++ react-class-components/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 class CustomButton extends React.Component {
   render() {
-    return <button>{ this.props.text }</button>;
+    return <button>{this.props.text}</button>;
   }
 }
 
