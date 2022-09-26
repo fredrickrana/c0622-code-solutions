75e8a2161fd075030088dd623645669768af8192
Fredrick Rana <fredrickrana01@gmail.com>
2022-09-26 21:40:19 +0000

    revised big-o-notation.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -1,17 +1,17 @@
 /* exported uniqueQuadratic, uniqueLinear */
 
 function uniqueLinear(words) {
-  const seen = {};                  // 1 * 1 = O(1)
+  const seen = {};                  // 1 * 1 = O(1) // constant time
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n) ~= 0(n)
+    i < words.length;               // 2 * n = O(2n) ~= 0(n) // linear time
     i++                             // 2 * n = O(2n) ~= 0(n)
   ) {
-    const word = words[i];          // 3 * n = O(3n) ~= 0(n)
+    const word = words[i];          // 2 * n = O(2n) ~= 0(n)
     if (!seen[word]) {              // 3 * n = O(3n) ~= 0(n)
-      seen[word] = true;            // 3 * n * 1 = O(3n) ~= 0(n)
-      unique[unique.length] = word; // 3 * n * 1 = O(3n) ~= 0(n)
+      seen[word] = true;            // 2 * n = O(2n) ~= 0(n)
+      unique[unique.length] = word; // 3 * n = O(3n) ~= 0(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
@@ -21,23 +21,23 @@ function uniqueQuadratic(words) {
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n) ~= 0(n)
+    i < words.length;               // 3 * n = O(3n) ~= 0(n)
     i++                             // 2 * n = O(2n) ~= 0(n)
   ) {
-    const word = words[i];          // 3 * n = O(3n) ~= 0(n)
-    let isUnique = true;            // 1 * 1 = O(1)
+    const word = words[i];          // 2 * n = O(2n) ~= 0(n)
+    let isUnique = true;            // 1 * n = O(n)
     for (
       let c = 0;                    // 1 * n = O(n) NESTED LOOP!
-      c < i;                        // 2 * n * n = O(2n^2) ~= 0(n^2)
+      c < i;                        // 2 * n * n = O(2n^2) ~= 0(n^2) // quadratic time
       c++                           // 2 * n * n = O(2n^2) ~= 0(n^2)
     ) {
-      const comparing = words[c];   // 3 * n = O(3n) ~- 0(n)
-      if (comparing === word) {     // 1 * n * n = O(n^2)
-        isUnique = false;           // 1 * 1 = O(1)
+      const comparing = words[c];   // 2 * n * n = O(2n^2) ~= 0(n^2)
+      if (comparing === word) {     // 2 * n * n = O(2n^2) ~= 0(n^2)
+        isUnique = false;           // 1 * n * n = O(n^2)
       }
     }
-    if (isUnique) {                 // 1 * 1 = O(1)
-      unique[unique.length] = word; // 3 * n * 1 = O(3n) ~= 0(n)
+    if (isUnique) {                 // 1 * n = O(n)
+      unique[unique.length] = word; // 3 * n = O(3n) ~= 0(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
